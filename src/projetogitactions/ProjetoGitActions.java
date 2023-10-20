f7037a13ef728ed9bcfbf29b6948f77be275056b
Maicon dos Santos <santos.maicon@gmail.com>
2023-10-20 20:39:01 -0300

    Update ProjetoGitActions.java

--- src/projetogitactions/ProjetoGitActions.java
+++ src/projetogitactions/ProjetoGitActions.java
@@ -15,7 +15,7 @@ public class ProjetoGitActions {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Projeto construido com sucesso!!!");
+        System.out;
     }
     
 }
